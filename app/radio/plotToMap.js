02985d9f11926ec1e86c570c3f4af3244e3d9291
SteepAtticStairs <lawlorcube06@gmail.com>
2023-01-19 22:40:03 -0500

    added some weather radio streams

--- app/radio/plotToMap.js
+++ app/radio/plotToMap.js
@@ -1,14 +1,28 @@
 const nwrStations = require('./data/nwrStations');
+const radioStreams = require('./data/radioStreams');
 const turf = require('@turf/turf');
 const setLayerOrder = require('../radar/map/setLayerOrder');
+const ut = require('../radar/utils');
 var map = require('../radar/map/map');
+
+const radioBtnGreen = 'rgb(57, 161, 53)';
+const radioBtnRed = 'rgb(200, 44, 44)';
 
 function plotToMap() {
     console.log(nwrStations);
 
+    function checkIfHasLivestream(callsign) {
+        if (radioStreams[callsign] != undefined) {
+            return radioStreams[callsign].radiourl; //`${callsign}: ${radioStreams[callsign].radiourl}`;
+        } else {
+            return false;
+        }
+    }
+
     var coords = [];
     for (var i in nwrStations) {
         try {
+            nwrStations[i].hasUrl = checkIfHasLivestream(nwrStations[i].CALLSIGN);
             var point = turf.point([parseFloat(nwrStations[i].LON), parseFloat(nwrStations[i].LAT)], nwrStations[i]);
             coords.push(point);
         } catch (e) { /*console.warn(e)*/ }
@@ -26,42 +40,137 @@ function plotToMap() {
             'type': 'circle',
             'source': 'radioStationSource',
             'paint': {
-                'circle-radius': 4,
+                'circle-radius': [
+                    'case',
+                    ['==', ['get', 'hasUrl'], false],
+                    2,
+                    ['==', ['get', 'STATUS'], 'NORMAL'],
+                    4,
+                    4
+                ],
                 'circle-color': [
                     'case',
-                    ['==', ['get', 'STATUS'], 'NORMAL'],
+                    ['==', ['get', 'hasUrl'], false],
+                    '#82cfd9', // lighter blue
                     '#16a61a', // lighter green
-                    ['==', ['get', 'STATUS'], 'OUT OF SERVICE'],
-                    '#de1818', // lighter red
-                    'black'
+                    // ['==', ['get', 'STATUS'], 'NORMAL'],
+                    // '#16a61a', // lighter green
+                    // // ['==', ['get', 'STATUS'], 'OUT OF SERVICE'],
+                    // // '#de1818', // lighter red
+                    // 'black'
                 ],
-                'circle-stroke-width': 4,
+                'circle-stroke-width': [
+                    'case',
+                    ['==', ['get', 'hasUrl'], false],
+                    2,
+                    ['==', ['get', 'STATUS'], 'NORMAL'],
+                    4,
+                    4
+                ],
                 'circle-stroke-color': [
                     'case',
-                    ['==', ['get', 'STATUS'], 'NORMAL'],
+                    ['==', ['get', 'hasUrl'], false],
+                    '#64a7b0', // darker blue
                     '#107514', // darker green
-                    ['==', ['get', 'STATUS'], 'OUT OF SERVICE'],
-                    '#991111', // darker red
-                    'black'
+                    // ['==', ['get', 'STATUS'], 'NORMAL'],
+                    // '#107514', // darker green
+                    // // ['==', ['get', 'STATUS'], 'OUT OF SERVICE'],
+                    // // '#991111', // darker red
+                    // 'black'
                 ],
             }
         });
         setLayerOrder();
 
+        var sound = document.createElement('audio');
+        sound.id = 'radioAudio';
+        document.body.appendChild(sound);
+
+        function displayErrorModal(content) {
+            ut.displayAtticDialog({
+                'title': 'Error',
+                'body': content,
+                'color': '#ba3043',
+                'textColor': 'white',
+            })
+        }
+
+        $('#radioAudio').on('error', function (e) {
+            // audio playback failed - show a message saying why
+            switch (e.target.error.code) {
+                case e.target.error.MEDIA_ERR_ABORTED:
+                    displayErrorModal('You aborted the video playback.');
+                    break;
+                case e.target.error.MEDIA_ERR_NETWORK:
+                    displayErrorModal('A network error caused the audio download to fail.');
+                    break;
+                case e.target.error.MEDIA_ERR_DECODE:
+                    displayErrorModal('The audio playback was aborted due to a corruption problem or because the video used features your browser did not support.');
+                    break;
+                case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+                    displayErrorModal('The video audio not be loaded, either because the server or network failed or because the format is not supported.');
+                    break;
+                default:
+                    displayErrorModal('An unknown error occurred.');
+                    break;
+            }
+        });
+
         map.on('click', 'radioStationLayer', (e) => {
             // console.log(`${e.features[0].properties.LAT}, ${e.features[0].properties.LON}`);
             const properties = e.features[0].properties;
+            const callsign = properties.CALLSIGN;
+            const radioStreamURL = checkIfHasLivestream(callsign);
             console.log(properties);
+
+            var initColor;
+            var iconType;
+            if (window.currentStreamURL == radioStreamURL) {
+                initColor = radioBtnRed;
+                iconType = 'pause';
+            } else {
+                initColor = radioBtnGreen;
+                iconType = 'play';
+            }
+            //var radioPlayBtn = `<button id="radioPlayBtn" url="${radioStreamURL}">Play</button>`;
+            var radioPlayBtn = `<div id="radioPlayBtn" class="radioPausePlayBtn" style="color: ${initColor};" url="${radioStreamURL}"><i id="radioPlayBtnIcon" class="fa-solid fa-${iconType}"></i></div>`;
+            if (radioStreamURL == false) { radioPlayBtn = '' }
 
             const popupContents = `
 <div><b>${properties.CALLSIGN}</b></div>
-<div>${properties.FREQ}</div>
-<div>${properties.SITENAME}, ${properties.SITESTATE}</div>`
+<div>${properties.FREQ} MHz</div>
+<div>${properties.SITENAME}, ${properties.SITESTATE}</div>
+${radioPlayBtn}`
 
             const popup = new mapboxgl.Popup({ className: 'alertPopup', maxWidth: '1000' })
                 .setLngLat([properties.LON, properties.LAT])
                 .setHTML(popupContents)
                 .addTo(map);
+
+            $('#radioPlayBtn').click((e) => {
+                const radioPlayBtnIcon = document.getElementById('radioPlayBtnIcon');
+                const radioPlayBtn = document.getElementById('radioPlayBtn');
+
+                const btnColor = radioPlayBtn.style.color;
+                const streamURL = radioPlayBtn.getAttribute('url');
+                var radioAudioElem = document.getElementById('radioAudio');
+
+                if (btnColor == radioBtnGreen) {
+                    radioPlayBtn.style.color = radioBtnRed;
+                    $('#radioPlayBtnIcon').removeClass('fa-play').addClass('fa-pause');
+
+                    if (streamURL != 'false') {
+                        window.currentStreamURL = streamURL;
+                        radioAudioElem.src = streamURL;
+                        radioAudioElem.play();
+                    }
+                } else {
+                    radioPlayBtn.style.color = radioBtnGreen
+                    $('#radioPlayBtnIcon').removeClass('fa-pause').addClass('fa-play');
+
+                    radioAudioElem.pause();
+                }
+            })
         })
 
         map.on('mouseenter', 'radioStationLayer', () => {
