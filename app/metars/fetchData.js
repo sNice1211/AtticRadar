f3b14608a4e96326c5b834af563a3f2a88b1466a
SteepAtticStairs <lawlorcube06@gmail.com>
2023-04-23 13:20:30 -0400

    fixed metar bug

--- app/metars/fetchData.js
+++ app/metars/fetchData.js
@@ -16,23 +16,23 @@ function xhrGzipFile(url, cb) {
 }
 
 function fetchMETARData(action) {
-    var curStation = $('#dataDiv').data('currentStation');
-    $('#dataDiv').data('currentMetarRadarStation', curStation);
+    // var curStation = $('#dataDiv').data('currentStation');
+    // $('#dataDiv').data('currentMetarRadarStation', curStation);
 
-    var distance = 250000;
-    var distanceMiles = distance / 1609;
-    var stationLat = radarStations[curStation][1];
-    var stationLon = radarStations[curStation][2];
+    // var distance = 250000;
+    // var distanceMiles = distance / 1609;
+    // var stationLat = radarStations[curStation][1];
+    // var stationLon = radarStations[curStation][2];
 
-    var bbx = geolib.getBoundsOfDistance(
-        { latitude: stationLat, longitude: stationLon },
-        distance
-    );
+    // var bbx = geolib.getBoundsOfDistance(
+    //     { latitude: stationLat, longitude: stationLon },
+    //     distance
+    // );
 
-    var minLat = bbx[0].latitude;
-    var minLon = bbx[0].longitude;
-    var maxLat = bbx[1].latitude;
-    var maxLon = bbx[1].longitude;
+    // var minLat = bbx[0].latitude;
+    // var minLon = bbx[0].longitude;
+    // var maxLat = bbx[1].latitude;
+    // var maxLon = bbx[1].longitude;
 
     //var url = `https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&minLat=${minLat}&minLon=${minLon}&maxLat=${maxLat}&maxLon=${maxLon}&hoursBeforeNow=3#`;
     //var url = `https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=~us&hoursBeforeNow=3#`;
