957bd21fd52eae050fbe4c09b21bb89102a6972e
SteepAtticStairs <lawlorcube06@gmail.com>
2023-05-13 19:15:24 -0400

    used chroma-js in the temp_colors file

--- dist/bundle.js
+++ dist/bundle.js
@@ -2423,9 +2423,11 @@ module.exports = {
 };
 },{"luxon":99}],21:[function(require,module,exports){
 const ut = require('../../radar/utils');
+const chroma = require('chroma-js');
 
-const tempColorObj = {
-    'low': 'rgb(247, 198, 251)',
+const low_temp = 'rgb(247, 198, 251)';
+const high_temp = 'rgb(163, 8, 148)';
+const temp_colors_dictionary = {
     '10': 'rgb(204, 120, 214)',
     '20': 'rgb(137, 67, 177)',
     '30': 'rgb(55, 30, 149)',
@@ -2437,125 +2439,43 @@ const tempColorObj = {
     '90': 'rgb(192, 56, 30)',
     '100': 'rgb(237, 14, 133)',
     '110': 'rgb(237, 14, 215)',
-    'high': 'rgb(163, 8, 148)'
+}
+const temperatures = Object.keys(temp_colors_dictionary).map(Number);
+const colors = Object.values(temp_colors_dictionary);
+
+const min_temp = Math.min(...temperatures);
+const max_temp = Math.max(...temperatures);
+
+const chroma_scale = chroma.scale(colors).domain(temperatures).mode('lab');
+
+function _determine_contrast(color) {
+    // this should actually be 0.4, i believe
+    return chroma(color).luminance() > 0.3 ? 'black' : 'white';
 }
 
-// https://stackoverflow.com/a/24253254
-function getColorGradientValue(p, rgb_beginning, rgb_end){
-    var w = p * 2 - 1;
+function getTempColor(temp_val) {
+    temp_val = parseInt(temp_val);
 
-    var w1 = (w + 1) / 2.0;
-    var w2 = 1 - w1;
-
-    var rgb = {
-        r: parseInt(rgb_beginning[0] * w1 + rgb_end[0] * w2),
-        g: parseInt(rgb_beginning[1] * w1 + rgb_end[1] * w2),
-        b: parseInt(rgb_beginning[2] * w1 + rgb_end[2] * w2)
-    };
-    return rgb;
-};
-
-function rgbValToArray(rgbString) {
-    return rgbString.replace('rgb(', '').replace(')', '').split(', ')
-}
-
-// https://stackoverflow.com/a/5624139
-function rgbToHex(r, g, b) {
-    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-// https://stackoverflow.com/a/11868159
-function colorLightOrDark(R, G, B) {
-    // https://stackoverflow.com/a/4726403
-    // var nThreshold = 105;
-    // var bgDelta = (R * 0.299) + (G * 0.587) + (B * 0.114);
-    // return ((255 - bgDelta) < nThreshold) ? "#000000" : "#ffffff"; 
-
-    // http://www.w3.org/TR/AERT#color-contrast
-    const brightness = Math.round((
-        (parseInt(R) * 299) +
-        (parseInt(G) * 587) +
-        (parseInt(B) * 114)) / 1000);
-    const textColour = (brightness > 125) ? 'black' : 'white';
-    return textColour;
-}
-
-function returnFullColorArray() {
-    var allKeys = Object.keys(tempColorObj);
-    allKeys = allKeys.map(element => Number(element)).filter(number => !Number.isNaN(number));
-    const minTemp = Math.min(...allKeys);
-    const maxTemp = Math.max(...allKeys);
-
-    var allTempColorVals = {};
-
-    for (var i = minTemp; i <= maxTemp; i++) {
-        if (i % 10 != 0) {
-            var lowestTen = parseInt(i / 10, 10) * 10;
-            var highestTen = (parseInt(i / 10, 10) + 1) * 10;
-
-            var lastDigit = i % 10;
-            var reversedVal = 10 - lastDigit;
-            reversedVal = parseFloat(`0.${reversedVal}`);
-            //console.log([lowestTen, i, highestTen])
-
-            var startColor = rgbValToArray(tempColorObj[lowestTen]);
-            var endColor = rgbValToArray(tempColorObj[highestTen]);
-
-            var { r, g, b } = getColorGradientValue(reversedVal, startColor, endColor);
-            var lightOrDark = colorLightOrDark(r, g, b);
-            var stringifiedRGB = `rgb(${r}, ${g}, ${b})`;
-
-            allTempColorVals[i] = [stringifiedRGB, lightOrDark];
-            //ut.colorLog(i, stringifiedRGB);
-            //ut.colorLog(`   ${i}   `, lightOrDark, `background-color: ${stringifiedRGB}`);
-        } else {
-            var curRGBVal = tempColorObj[i];
-            var rgbArr = rgbValToArray(curRGBVal);
-            var lightOrDark = colorLightOrDark(rgbArr[0], rgbArr[1], rgbArr[2]);
-            allTempColorVals[i] = [curRGBVal, lightOrDark];
-            //ut.colorLog(i, curRGBVal);
-            //ut.colorLog(`   ${i}   `, lightOrDark, `background-color: ${curRGBVal}`);
-        }
-    }
-    var lowTempReturnVal = tempColorObj.low;
-    var rgbArr = rgbValToArray(lowTempReturnVal);
-    var lightOrDark = colorLightOrDark(rgbArr[0], rgbArr[1], rgbArr[2]);
-    lowTempReturnVal = [lowTempReturnVal, lightOrDark];
-
-    var highTempReturnVal = tempColorObj.high;
-    var rgbArr = rgbValToArray(highTempReturnVal);
-    var lightOrDark = colorLightOrDark(rgbArr[0], rgbArr[1], rgbArr[2]);
-    highTempReturnVal = [highTempReturnVal, lightOrDark];
-
-    return {
-        'fullObj': allTempColorVals,
-        'minTemp': minTemp,
-        'maxTemp': maxTemp,
-        'lowTempColor': lowTempReturnVal,
-        'highTempColor': highTempReturnVal
-    };
-}
-
-function getTempColor(tempVal) {
-    tempVal = parseInt(tempVal);
-    var allColors = returnFullColorArray();
-
-    if (tempVal < allColors.minTemp) {
-        return allColors.lowTempColor;
-    } else if (tempVal > allColors.maxTemp) {
-        return allColors.highTempColor;
+    var color;
+    if (temp_val < min_temp) {
+        color = low_temp;
+    } else if (temp_val > max_temp) {
+        color = high_temp;
     } else {
-        return allColors.fullObj[tempVal];
+        color = chroma_scale(temp_val);
     }
-}
 
-// ut.colorLog(60, getTempColor(60));
-// ut.colorLog(70, getTempColor(70));
-// ut.colorLog(80, getTempColor(80));
-// ut.colorLog(90, getTempColor(90));
-// ut.colorLog(100, getTempColor(100));
+    const contrast_color = _determine_contrast(color);
+
+    return [color, contrast_color];
+    // return {
+    //     'background_color': color,
+    //     'text_color': _determine_contrast(color)
+    // }
+}
 
 module.exports = getTempColor;
-},{"../../radar/utils":76}],22:[function(require,module,exports){
+},{"../../radar/utils":76,"chroma-js":98}],22:[function(require,module,exports){
 const ut = require('../radar/utils');
 
 // https://github.com/mapbox/mapbox-gl-js/issues/2848#issuecomment-354447882
