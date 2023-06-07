a4f5b37e11cbe4357aa17e89a66753ac57ff3a46
SteepAtticStairs <lawlorcube06@gmail.com>
2023-06-06 22:36:33 -0400

    fixed city names not being visible

--- dist/bundle.js
+++ dist/bundle.js
@@ -2032,52 +2032,52 @@ module.exports = {
 },{"./map":13}],15:[function(require,module,exports){
 var map = require('./map');
 
-function moveLayerToTop(layerName) {
-    if (map.getLayer(layerName)) {
-        map.moveLayer(layerName);
+function move_layer_to_top(layer_name, before_layer = undefined) {
+    if (map.getLayer(layer_name)) {
+        if (before_layer == undefined) {
+            map.moveLayer(layer_name);
+        } else {
+            map.moveLayer(layer_name, before_layer);
+        }
     }
 }
 
 function setLayerOrder() {
-    moveLayerToTop('spc_fill');
-    moveLayerToTop('spc_border');
+    const before_layer = 'land-structure-line';
 
-    moveLayerToTop('baseReflectivity');
-    moveLayerToTop('radioStationLayer');
+    move_layer_to_top('spc_fill', before_layer);
+    move_layer_to_top('spc_border', before_layer);
 
-    // var isRoadsStreetsVisChecked = $('#armrRoadsStreetsVisBtnSwitchElem').is(':checked');
-    // if (!isRoadsStreetsVisChecked) {
-    //     setBaseMapLayers('cities');
-    // } else if (isRoadsStreetsVisChecked) {
-    //     setBaseMapLayers('both');
-    // }
+    move_layer_to_top('baseReflectivity', before_layer);
 
-    moveLayerToTop('alertsLayerOutline');
-    moveLayerToTop('alertsLayer');
-    moveLayerToTop('alertsLayerFill');
+    move_layer_to_top('radioStationLayer');
+
+    move_layer_to_top('alertsLayerOutline');
+    move_layer_to_top('alertsLayer');
+    move_layer_to_top('alertsLayerFill');
     var storm_track_layers = window.atticData.storm_track_layers;
     if (storm_track_layers != undefined) {
         for (var i in storm_track_layers) {
-            moveLayerToTop(storm_track_layers[i]);
+            move_layer_to_top(storm_track_layers[i]);
         }
     }
     var tvs_layers = window.atticData.tvs_layers;
     if (tvs_layers != undefined) {
         for (var i in tvs_layers) {
-            moveLayerToTop(tvs_layers[i]);
+            move_layer_to_top(tvs_layers[i]);
         }
     }
 
-    moveLayerToTop('metarSymbolLayer');
+    move_layer_to_top('metarSymbolLayer');
 
-    moveLayerToTop('lightningLayer');
+    move_layer_to_top('lightningLayer');
 
-    moveLayerToTop('stationSymbolLayer');
+    move_layer_to_top('stationSymbolLayer');
 
     var surface_fronts_layers = window.atticData.surface_fronts_layers;
     if (surface_fronts_layers != undefined) {
         for (var i in surface_fronts_layers) {
-            moveLayerToTop(surface_fronts_layers[i]);
+            move_layer_to_top(surface_fronts_layers[i]);
         }
     }
 }
