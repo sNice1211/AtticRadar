2dd87ab02899e4e62ac26e2ca1f8abaef9582a3b
SteepAtticStairs <lawlorcube06@gmail.com>
2023-06-12 12:07:34 -0400

    SPC rows now show valid time for each outlook

--- app/spc/fetch_data.js
+++ app/spc/fetch_data.js
@@ -2,18 +2,59 @@ const ut = require('../core/utils');
 const urls = require('./urls');
 const plot_data = require('./plot_data');
 const turf = require('@turf/turf');
+const luxon = require('luxon');
+
+function _return_time_range(json) {
+    var issue;
+    var expire;
+    turf.propEach(json, (current_properties, feature_index) => {
+        expire = current_properties.EXPIRE;
+        issue = current_properties.ISSUE;
+    });
+
+    function _parse_format_date_string(date_string) {
+        const date = luxon.DateTime.fromFormat(date_string, 'yyyyMMddHHmm', { zone: 'UTC' });
+        const formatted_date = date.toLocal().toFormat('EEE MMM d, h:mm a'); // EEE h:mm a
+        return formatted_date;
+    }
+
+    const issue_formatted = _parse_format_date_string(issue);
+    const expire_formatted = _parse_format_date_string(expire);
+    const full_formatted = `${issue_formatted} - ${expire_formatted}`;
+    return [issue_formatted, expire_formatted];
+}
+
+function _return_time_range_html(issue_formatted, expire_formatted) {
+    return `\
+<p style="margin: 0px; font-size: 11px">&nbsp;&nbsp;&nbsp;${issue_formatted} thru</p>\
+<p style="margin: 0px; font-size: 11px">&nbsp;&nbsp;&nbsp;${expire_formatted}</p>`;
+}
 
 function fetch_spc_data(type, category, day) {
+    const elem = $(`#armrSPC_${type}-${category}-${day}_BtnSwitchElem`);
+    const atticRadarMenuRow = elem.closest('.atticRadarMenuRow');
+    atticRadarMenuRow.append(`<i class="helperText" style="opacity: 50%">&nbsp;&nbsp;&nbsp;Loading...</i>`);
+
     fetch(ut.phpProxy + urls[type][category][day])
     .then(response => response.json())
     .then(json => {
+        const [issue_formatted, expire_formatted] = _return_time_range(json);
+
+        // atticRadarMenuRow.find('.helperText').remove();
+        $('.helperText').remove();
         if (turf.coordAll(json).length == 0) {
-            const elem = $(`#armrSPC_${type}-${category}-${day}_BtnSwitchElem`);
-            elem.closest('.atticRadarMenuRow')
+            atticRadarMenuRow
                 .addClass('atticRadarMenuRowDisabled')
-                .append(`&nbsp;<i style="opacity: 50%">(no data)</i>`);
+                .append(
+`<i class="helperTextDisabled" style="opacity: 50%">\
+<p style="margin: 0px; font-size: 11px; font-weight: bold">&nbsp;&nbsp;&nbsp;NO DATA</p>\
+${_return_time_range_html(issue_formatted, expire_formatted)}\
+</i>`
+);
             elem[0].checked = false;
         } else {
+            atticRadarMenuRow
+                .append(`<i class="helperText" style="opacity: 50%">${_return_time_range_html(issue_formatted, expire_formatted)}</i>`);
             plot_data(json);
         }
     })
--- dist/bundle.js
+++ dist/bundle.js
@@ -29161,25 +29161,66 @@ const ut = require('../core/utils');
 const urls = require('./urls');
 const plot_data = require('./plot_data');
 const turf = require('@turf/turf');
+const luxon = require('luxon');
+
+function _return_time_range(json) {
+    var issue;
+    var expire;
+    turf.propEach(json, (current_properties, feature_index) => {
+        expire = current_properties.EXPIRE;
+        issue = current_properties.ISSUE;
+    });
+
+    function _parse_format_date_string(date_string) {
+        const date = luxon.DateTime.fromFormat(date_string, 'yyyyMMddHHmm', { zone: 'UTC' });
+        const formatted_date = date.toLocal().toFormat('EEE MMM d, h:mm a'); // EEE h:mm a
+        return formatted_date;
+    }
+
+    const issue_formatted = _parse_format_date_string(issue);
+    const expire_formatted = _parse_format_date_string(expire);
+    const full_formatted = `${issue_formatted} - ${expire_formatted}`;
+    return [issue_formatted, expire_formatted];
+}
+
+function _return_time_range_html(issue_formatted, expire_formatted) {
+    return `\
+<p style="margin: 0px; font-size: 11px">&nbsp;&nbsp;&nbsp;${issue_formatted} thru</p>\
+<p style="margin: 0px; font-size: 11px">&nbsp;&nbsp;&nbsp;${expire_formatted}</p>`;
+}
 
 function fetch_spc_data(type, category, day) {
+    const elem = $(`#armrSPC_${type}-${category}-${day}_BtnSwitchElem`);
+    const atticRadarMenuRow = elem.closest('.atticRadarMenuRow');
+    atticRadarMenuRow.append(`<i class="helperText" style="opacity: 50%">&nbsp;&nbsp;&nbsp;Loading...</i>`);
+
     fetch(ut.phpProxy + urls[type][category][day])
     .then(response => response.json())
     .then(json => {
+        const [issue_formatted, expire_formatted] = _return_time_range(json);
+
+        // atticRadarMenuRow.find('.helperText').remove();
+        $('.helperText').remove();
         if (turf.coordAll(json).length == 0) {
-            const elem = $(`#armrSPC_${type}-${category}-${day}_BtnSwitchElem`);
-            elem.closest('.atticRadarMenuRow')
+            atticRadarMenuRow
                 .addClass('atticRadarMenuRowDisabled')
-                .append(`&nbsp;<i style="opacity: 50%">(no data)</i>`);
+                .append(
+`<i class="helperTextDisabled" style="opacity: 50%">\
+<p style="margin: 0px; font-size: 11px; font-weight: bold">&nbsp;&nbsp;&nbsp;NO DATA</p>\
+${_return_time_range_html(issue_formatted, expire_formatted)}\
+</i>`
+);
             elem[0].checked = false;
         } else {
+            atticRadarMenuRow
+                .append(`<i class="helperText" style="opacity: 50%">${_return_time_range_html(issue_formatted, expire_formatted)}</i>`);
             plot_data(json);
         }
     })
 }
 
 module.exports = fetch_spc_data;
-},{"../core/utils":30,"./plot_data":89,"./urls":90,"@turf/turf":107}],88:[function(require,module,exports){
+},{"../core/utils":30,"./plot_data":89,"./urls":90,"@turf/turf":107,"luxon":113}],88:[function(require,module,exports){
 const map = require('../core/map/map');
 const armFunctions = require('../core/menu/atticRadarMenu');
 const fetch_spc_data = require('./fetch_data');
