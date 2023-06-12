fc3fabc81fe226fa0b6b9908aa3939bc2e062e1d
SteepAtticStairs <lawlorcube06@gmail.com>
2023-06-11 22:16:54 -0400

    disable SPC rows with no data

--- app/spc/fetch_data.js
+++ app/spc/fetch_data.js
@@ -1,12 +1,21 @@
 const ut = require('../core/utils');
 const urls = require('./urls');
 const plot_data = require('./plot_data');
+const turf = require('@turf/turf');
 
 function fetch_spc_data(type, category, day) {
     fetch(ut.phpProxy + urls[type][category][day])
     .then(response => response.json())
     .then(json => {
-        plot_data(json);
+        if (turf.coordAll(json).length == 0) {
+            const elem = $(`#armrSPC_${type}-${category}-${day}_BtnSwitchElem`);
+            elem.closest('.atticRadarMenuRow')
+                .addClass('atticRadarMenuRowDisabled')
+                .append(`&nbsp;<i style="opacity: 50%">(no data)</i>`);
+            elem[0].checked = false;
+        } else {
+            plot_data(json);
+        }
     })
 }
 
--- dist/bundle.js
+++ dist/bundle.js
@@ -29160,17 +29160,26 @@ module.exports = plotToMap;
 const ut = require('../core/utils');
 const urls = require('./urls');
 const plot_data = require('./plot_data');
+const turf = require('@turf/turf');
 
 function fetch_spc_data(type, category, day) {
     fetch(ut.phpProxy + urls[type][category][day])
     .then(response => response.json())
     .then(json => {
-        plot_data(json);
+        if (turf.coordAll(json).length == 0) {
+            const elem = $(`#armrSPC_${type}-${category}-${day}_BtnSwitchElem`);
+            elem.closest('.atticRadarMenuRow')
+                .addClass('atticRadarMenuRowDisabled')
+                .append(`&nbsp;<i style="opacity: 50%">(no data)</i>`);
+            elem[0].checked = false;
+        } else {
+            plot_data(json);
+        }
     })
 }
 
 module.exports = fetch_spc_data;
-},{"../core/utils":30,"./plot_data":89,"./urls":90}],88:[function(require,module,exports){
+},{"../core/utils":30,"./plot_data":89,"./urls":90,"@turf/turf":107}],88:[function(require,module,exports){
 const map = require('../core/map/map');
 const armFunctions = require('../core/menu/atticRadarMenu');
 const fetch_spc_data = require('./fetch_data');
