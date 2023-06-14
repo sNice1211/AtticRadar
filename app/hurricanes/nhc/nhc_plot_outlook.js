cda6359ceae23b08a6b4e4dd4e8c32a04d0f22ff
SteepAtticStairs <lawlorcube06@gmail.com>
2023-06-14 14:36:10 -0400

    made the NHC outlook code look nicer

--- app/hurricanes/nhc/nhc_plot_outlook.js
+++ app/hurricanes/nhc/nhc_plot_outlook.js
@@ -1,17 +1,10 @@
 const kmz_to_geojson = require('../kmz_to_geojson');
 const map = require('../../core/map/map');
 
-function showPopup(e, coordsFromFeatureOrClick) {
-    var lat;
-    var lng;
-    if (coordsFromFeatureOrClick == 'feature') {
-        lng = e.features[0].geometry.coordinates[0];
-        lat = e.features[0].geometry.coordinates[1];
-    } else if (coordsFromFeatureOrClick == 'click') {
-        lng = e.lngLat.lng;
-        lat = e.lngLat.lat;
-    }
-    var properties = e.features[0].properties;
+function _click_listener(e) {
+    const lng = e.lngLat.lng;
+    const lat = e.lngLat.lat;
+    const properties = e.features[0].properties;
 
     var percentage_html = '';
     if (properties.hasOwnProperty('2day_percentage')) {
@@ -24,7 +17,7 @@ function showPopup(e, coordsFromFeatureOrClick) {
         percentage_html += `<div style="font-size: 20px; text-align: center">7-day: <b>${properties['7day_percentage']}%</b></div>`;
     }
 
-    var popupContent =
+    const popup_content =
         `<div style="overflow-y: scroll; max-height: 150px; color: white">
             ${percentage_html}
             <br>
@@ -35,7 +28,7 @@ function showPopup(e, coordsFromFeatureOrClick) {
 
     new mapboxgl.Popup({ className: 'alertPopup'})
         .setLngLat([lng, lat])
-        .setHTML(popupContent)
+        .setHTML(popup_content)
         //.setHTML(e.features[0].properties.description)
         .addTo(map);
 }
@@ -43,72 +36,86 @@ function showPopup(e, coordsFromFeatureOrClick) {
 function nhc_plot_outlook(kmz_blob, id) {
     kmz_to_geojson(kmz_blob, (geojson) => {
         for (var x = 0; x < geojson.features.length; x++) {
-            var curFeature = geojson.features[x];
-            var type = curFeature.geometry.type;
-            if (type == 'Polygon') {
-                var fillColor = curFeature.properties.fill;
-                var fillOpacity = curFeature.properties['fill-opacity'];
-                var borderColor = curFeature.properties.stroke;
-                var borderWidth = curFeature.properties['stroke-width'];
-                var coneCoordinates = curFeature.geometry.coordinates[0];
-                curFeature.geometry.coordinates[0].push(coneCoordinates[0]);
+            const cur_feature = geojson.features[x];
+            const type = cur_feature.geometry.type;
 
+            if (type == 'Polygon') {
+                const source_name = `hurricane_outlook_${x}${id}_source`;
+                const layer_name = `hurricane_outlook_${x}${id}_layer`;
+                const layer_outline_name = `hurricane_outlook_${x}${id}_outline`;
+
+                const fill_color = cur_feature.properties.fill;
+                const fill_opacity = cur_feature.properties['fill-opacity'];
+                const border_color = cur_feature.properties.stroke;
+                const border_width = cur_feature.properties['stroke-width'];
+                const cone_coordinates = cur_feature.geometry.coordinates[0];
+                cur_feature.geometry.coordinates[0].push(cone_coordinates[0]);
+
+                map.addSource(source_name, {
+                    type: 'geojson',
+                    data: cur_feature,
+                });
                 map.addLayer({
-                    'id': `outlookCone${id}${x}`,
+                    'id': layer_name,
                     'type': 'fill',
-                    'source': {
-                        type: 'geojson',
-                        data: curFeature,
-                    },
+                    'source': source_name,
                     paint: {
                         //#0080ff blue
                         //#ff7d7d red
-                        'fill-color': fillColor,
+                        'fill-color': fill_color,
                         'fill-opacity': 0.3
                     }
                 });
                 map.addLayer({
-                    'id': `outlookConeOutline${id}${x}`,
+                    'id': layer_outline_name,
                     'type': 'line',
-                    'source': `outlookCone${id}${x}`,
+                    'source': source_name,
                     'paint': {
                         //#014385 blue
                         //#850101 red
-                        'line-color': borderColor,
-                        'line-width': borderWidth
+                        'line-color': border_color,
+                        'line-width': border_width
                     }
                 });
-                window.atticData.hurricane_layers.push(`outlookCone${id}${x}`, `outlookConeOutline${id}${x}`);
+                window.atticData.hurricane_layers.push(source_name, layer_name, layer_outline_name);
 
-                map.on('mouseenter', `outlookCone${id}${x}`, function (e) { map.getCanvas().style.cursor = 'pointer'; })
-                map.on('mouseleave', `outlookCone${id}${x}`, function (e) { map.getCanvas().style.cursor = ''; })
-                map.on('click', `outlookCone${id}${x}`, function(e) { showPopup(e, 'click') });
+                map.on('mouseenter', layer_name, function (e) {
+                    map.getCanvas().style.cursor = 'pointer';
+                })
+                map.on('mouseleave', layer_name, function (e) {
+                    map.getCanvas().style.cursor = '';
+                })
+                map.on('click', layer_name, _click_listener);
+
             } else if (type == 'Point') {
-                if (curFeature.properties.name == undefined) curFeature.properties.name = '';
-                if (!(curFeature.properties.name.includes('Tropical cyclone formation is not expected')) && !(curFeature.properties.name.includes('during the next'))) {
-                    var black = 'rgb(0, 0, 0)';
-                    var highColor = 'rgb(214, 46, 31)'; // red
-                    var mediumColor = 'rgb(240, 151, 55)'; // orange
-                    var lowColor = 'rgb(255, 255, 84)'; // yellow
+                const source_name = `hurricane_outlook_point_${x}${id}_source`;
+                const layer_name = `hurricane_outlook_point_${x}${id}_layer`;
 
-                    if (curFeature.properties.styleUrl == '#highx') {
-                        curFeature.properties.color = highColor;
-                    } else if (curFeature.properties.styleUrl == '#medx') {
-                        curFeature.properties.color = mediumColor;
-                    } else if (curFeature.properties.styleUrl == '#lowx') {
-                        curFeature.properties.color = lowColor;
+                if (cur_feature.properties.name == undefined) cur_feature.properties.name = '';
+                if (!(cur_feature.properties.name.includes('Tropical cyclone formation is not expected')) && !(cur_feature.properties.name.includes('during the next'))) {
+                    const black = 'rgb(0, 0, 0)';
+                    const high_color = 'rgb(214, 46, 31)'; // red
+                    const medium_color = 'rgb(240, 151, 55)'; // orange
+                    const low_color = 'rgb(255, 255, 84)'; // yellow
+
+                    if (cur_feature.properties.styleUrl == '#highx') {
+                        cur_feature.properties.color = high_color;
+                    } else if (cur_feature.properties.styleUrl == '#medx') {
+                        cur_feature.properties.color = medium_color;
+                    } else if (cur_feature.properties.styleUrl == '#lowx') {
+                        cur_feature.properties.color = low_color;
                     } else {
-                        curFeature.properties.color = highColor;
+                        cur_feature.properties.color = high_color;
                     }
 
-                    map.addSource(`outlookPointSource${id}${x}`, {
+                    map.addSource(source_name, {
                         type: 'geojson',
-                        data: curFeature
+                        data: cur_feature
                     });
                     map.addLayer({
-                        'id': `outlookPoint${id}${x}`,
+                        'id': layer_name,
                         'type': 'circle',
-                        'source': `outlookPointSource${id}${x}`,
+                        'source': source_name,
                         'paint': {
                             'circle-radius': 9,
                             'circle-stroke-width': 2,
@@ -116,7 +123,7 @@ function nhc_plot_outlook(kmz_blob, id) {
                             'circle-stroke-color': black,
                         }
                     });
-                    window.atticData.hurricane_layers.push(`outlookPoint${id}${x}`, `outlookConeOutline${id}${x}`);
+                    window.atticData.hurricane_layers.push(source_name, layer_name);
                 }
             }
         }
