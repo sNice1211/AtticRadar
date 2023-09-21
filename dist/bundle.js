6605744a6b81244e89bcf3856caf2c60d58408a1
SteepAtticStairs <lawlorcube06@gmail.com>
2023-09-20 21:06:43 -0400

    added discord to my about screen

--- dist/bundle.js
+++ dist/bundle.js
@@ -20,11 +20,12 @@ Be sure to check there for updates and general info about the project.
 
 AtticRadar is free to use and doesn't have any ads. \
 I hope that you enjoy the app, and feel free to shoot me a message on Twitter if you have a question! \
-My email is also open: <a href="mailto:steepatticstairs@gmail.com" style="color: #53a2e0;">steepatticstairs@gmail.com</a>
+My email is also open: <a href="mailto:steepatticstairs@gmail.com" style="color: #53a2e0;">steepatticstairs@gmail.com</a> \
+You can also message me on Discord - my username is <code class="about_box_code">steepatticstairs</code>.
 
 Are you looking for AtticRadar's source code?
 Due to previous use of my code that I wasn't comfortable with, I have unfortunately decided to make the source code private, for the time being. \
-However, if you believe you have a legitimate reason for viewing the code, feel free to send me an email or a message explaining why, and I'll consider it. \
+However, if you believe you have a legitimate reason for viewing the code, feel free to send me a message or an message explaining why, and I'll consider it. \
 Thanks!
 
 Copyright © 2023 SteepAtticStairs. All rights reserved.
